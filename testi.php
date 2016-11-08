221eca357355d249ab199a2587e11f82df9ab453
t4pama01@students.oamk.fi <t4pama01@students.oamk.fi>
2016-11-08 09:12:09 +0200

    Testitiedosto luotu

---
+++ testi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testisivu</title>
+</head>
+<body>
+
+<h1>Testataan PHP:tä</h1>
+<?php
+$x=5;
+echo 'Muuttujan arvo on '.$x;
+echo '<br>';
+echo "Muuttujan arvo on $x";
+?>
+
+</body>
+</html>
